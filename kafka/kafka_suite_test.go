4eb7ec03b6f3ef8608d8b95e21a3c60765cbf3be
David ALEXANDRE <david.alexandre@w6d.io>
2022-11-20 23:13:39 +0100

    perf: add integration tests

--- kafka/kafka_suite_test.go
+++ kafka/kafka_suite_test.go
@@ -1,3 +1,5 @@
+//go:build !integration
+
 /*
 Copyright 2020 WILDCARD
 
@@ -5,7 +7,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,16 +16,17 @@ See the License for the specific language governing permissions and
 limitations under the License.
 Created on 08/02/2021
 */
+
 package kafka_test
 
 import (
-	"testing"
+    "testing"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+    . "github.com/onsi/ginkgo/v2"
+    . "github.com/onsi/gomega"
 )
 
 func TestHook(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Kafka Suite")
+    RegisterFailHandler(Fail)
+    RunSpecs(t, "Kafka Suite")
 }
---
+++ tests/hook_integration_test.go
@@ -0,0 +1,66 @@
+//go:build integration
+
+/*
+Copyright 2020 WILDCARD
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+Created on 19/11/2022
+*/
+
+package test
+
+import (
+    "fmt"
+    "github.com/w6d-io/x/logx"
+    "os/exec"
+
+    . "github.com/onsi/ginkgo/v2"
+    . "github.com/onsi/gomega"
+
+    "github.com/w6d-io/hook"
+)
+
+var _ = Describe("", func() {
+    Context("Test hook with RedPanda", func() {
+        var payload = struct {
+            Data string
+        }{
+            Data: "test",
+        }
+        var topic = "int-test"
+        BeforeEach(func() {
+        })
+        AfterEach(func() {
+        })
+        It("produces successfully", func() {
+            By("init kafka subscription", func() {
+                Expect(hook.Subscribe(ctx, fmt.Sprintf("kafka://%s?topic=%s&protocol=SASL_SSL&mechanisms=PLAIN&messagekey=test", kafkaHost, topic), "*")).To(Succeed())
+            })
+
+            By("Send payload")
+            err := hook.Send(ctx, &payload, "send succeed")
+            Expect(err).ToNot(HaveOccurred())
+            By("consume the message", func() {
+                ps := exec.Command(rpkPath, "topic", "consume", topic, "--num", "1", "--brokers", kafkaHost)
+                out, err := ps.Output()
+                Expect(err).ToNot(HaveOccurred())
+                logx.WithName(ctx, "int-test").Info("test", "out", out)
+            })
+
+        })
+        It("misses the topic", func() {
+            Expect(hook.Subscribe(ctx, fmt.Sprintf("kafka://%s", kafkaHost), "*")).ToNot(Succeed())
+        })
+
+    })
+})
